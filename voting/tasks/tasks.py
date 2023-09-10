8effec0ab2c251b472c1cd2ce23dc14087a52256
Edwin Tijaro <e.tijaro@uniandes.edu.co>
2023-09-10 16:11:56 -0500

    logica de comparar resutlados en voting (rojo)

--- voting/tasks/tasks.py
+++ voting/tasks/tasks.py
@@ -1,7 +1,21 @@
 from celery import Celery
 import requests
+from collections import Counter
 
 app = Celery(__name__)
+
+def encontrar_resultado_diferente(resultados):
+    # Contar la frecuencia de cada resultado
+    frecuencias = Counter(resultados)
+
+    # Encontrar el resultado con frecuencia igual a 1 (el diferente)
+    resultado_diferente = None
+    for resultado, frecuencia in frecuencias.items():
+        if frecuencia == 1: #o int(len(resultados)/2)
+            resultado_diferente = resultado
+            break
+
+    return resultado_diferente
 
 @app.task
 def voting(resultados):
@@ -11,16 +25,14 @@ def voting(resultados):
     print(resultados_filtrados)
 
     if len(resultados_filtrados) >= 3:
-        # primer_resultado = None
-        # for objeto in resultados_filtrados:
-        #     resultado_actual = objeto["Resultado"]
-        #     if primer_resultado is None:
-        #         primer_resultado = resultado_actual
-        #     elif resultado_actual != primer_resultado:
-        #         segundo_resultado = objeto
-        #         break
-        #     elif segundo_resultado == resultado_actual:
-        #         objeto_diferente = primer_resultado
-        # print("Servicio fallando",objeto_diferente)
+        resultado_diferente = encontrar_resultado_diferente([res['Resultado'] for res in resultados_filtrados])
+        
+        if resultado_diferente is not None:
+            print("Hay un resultado diferente:", resultado_diferente)
+            # -
+        else:
+            print("Todos los resultados son iguales.")
+    else:
+        print("No hay suficientes resultados para verificar.")
  
         requests.delete("http://localhost:5002/rating/"+str(resultados[0]['id']))
